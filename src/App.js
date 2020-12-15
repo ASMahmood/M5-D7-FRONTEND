050f00b8d60ab919f30ca351ed213ac528d4af86
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-12-15 16:00:56 +0000

    trying and failing to fertch books from online server

--- src/App.js
+++ src/App.js
@@ -7,6 +7,29 @@ import scifi from "./scifi.json";
 import BookList from "./components/BookList";
 
 class App extends React.Component {
+  state = {
+    booklist: [],
+  };
+
+  componentDidMount = () => {
+    this.fetchBooks();
+  };
+
+  fetchBooks = async () => {
+    let response = await fetch(
+      "https://my-bookshop-is-better.herokuapp.com/books",
+      {
+        method: "GET",
+        headers: {
+          Origin: "http://localhost:3000",
+        },
+      }
+    );
+    let parsedJSON = await response.json();
+    console.log(parsedJSON);
+    console.log(response);
+  };
+
   render() {
     return (
       <div className="App">
